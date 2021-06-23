0d435c1a1aa530358cfb1ea59ee8412b5a8b5ec6
Ameya Kulkarni <kulkarni.ameya11@gmail.com>
2021-06-22 23:04:44 -0500

    Added descriptions for few projects

--- js/manimation.js
+++ js/manimation.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
   let data = [
     {
       'name': 'BluAge Corporation | Software Developer Intern ',
+      'subtitle':'',
       'logo': '',
       'content': ["Accomplished Digital Transformation of legacy application to modern architecture by transforming, debugging, and testing a high volume of code from COBOL to C\# and AngularJS.",
         "Mentored three new team members to ensure smooth onboarding to the team."
@@ -24,6 +25,7 @@ $(document).ready(function () {
       'link': 'https://www.bluage.com/'
     }, {
       'name': 'eQTechnologic | Software Engineer ',
+      'subtitle':'',
       'logo': '',
       'content': [" Acquired a critical customer in a short period of 3 months, by developing a connector prototype that achieved data migration, integration, and business intelligence use cases for Oracle Agile PLM For Process Application.",
         "Automated data model generation, by implementing recursive parsing algorithms to achieve extensible code and reduction in development time.",
@@ -33,13 +35,59 @@ $(document).ready(function () {
       'link': 'https://www.1eq.com/'
     }, {
       'name': 'GarageBot | Android Dev',
+      'subtitle':'Digital Assistant for Vehicle Service Stations ',
       'logo': '',
       'content': ["Designed and developed the entire Android Front-End infrastructure which reduces job card management time by 50\%, resulting in a 10\% rise in revenue, and is actively used by over 300 vehicle service stations daily.",
         "Increased application speed and availability, by implementing a caching and synchronization framework to reduce API calls by over 50\%, resulting in a rise in customers for the product."
         , "<b>Technologies :</b> Java, Android, SQLite <br><br><br><br><br><br><br><br><br><br>"],
-      'duration': 'Digital Assistant for Vehicle Service Stations<br>June 2017 – June 2019',
+      'duration': 'June 2017 – June 2019',
       'link': 'https://garagebot.in/html/'
+    }, {
+      'name': 'Database Implementation | Academic project',
+      'subtitle':'SQL Database implementation based on SQLite specification',
+      'logo': '',
+      'content': ["Implemented BTree based SQL database query parser and query execution engine.",
+        "Achieved execution of basic DDL and DML commands through command line interface by implementing relational database file management as per SQLite Database Specifications."
+        , "<b>Technologies :</b> Java, SQLite <br><br><br><br><br><br><br><br><br><br>"],
+      'duration': 'Jan 2020 – May 2020',
+      'link': 'https://github.com/ameyak11/dbimplementation'
+    } , {
+      'name': 'English - Hindi Translator | Machine Learning project',
+      'subtitle':'Convert text in English language to Hindi Language',
+      'logo': '',
+      'content': ["Developed RNN based LSTM Encoder-Decoder Translation model, compared it with existing open-source models, and achieved a cross-entropy error of 0.015 over the selected test dataset."
+        , "<b>Technologies :</b> Python, Keras, Pandas <br><br><br><br><br><br><br><br><br><br>"],
+      'duration': 'Jan 2020 – May 2020',
+      'link': 'https://github.com/ameyak11/machinetranslationmodel/'
+    }, {
+      'name': 'Morris Game | AI project',
+      'subtitle':'Aplha-Beta pruning based Morris Game Player',
+      'logo': '',
+      'content': ["Developed a minimax and aplha beta pruning based bot which takes a game state as input and outputs the best possible next state"
+        , "<b>Technologies :</b> Java <br><br><br><br><br><br><br><br><br><br>"],
+      'duration': 'May 2020 – Aug 2020',
+      'link': 'https://github.com/ameyak11/MorrisGamePlayer'
+    },{
+      'name': 'Varta (Smart News) | Award Winning ML Project',
+      'subtitle':'Sentence extraction based news article summary generator',
+      'logo': '',
+      'content': ["Designed and implemented the entire data pipeline using flask to feed news article input to the NLP model that summarized the article with 64% accuracy."
+        ,"Achieved personalized news feed by implementing a news recommendation system based on user profile and user activity through an Android application front end."  
+        ,"State level 'Best Project Award' by ISSRD."
+        ,"Second runner up in AI at National IEEE symposium."
+        , "<b>Technologies :</b> Python, Python Flask, BeautifulSoup4, Celery, MongoDB <br><br><br><br><br><br><br>"],
+      'duration': 'June 2016 – June 2017',
+      'link': 'https://github.com/ameyak11/textsummarizer/'
+    } , {
+      'name': 'Creative Prims | Fun project',
+      'subtitle':'Maze generation using prims algorithm',
+      'logo': '',
+      'content': ["This was a fun project to illustrate maze generation by tweaking prims algorithm"
+        , "<b>Technologies :</b> Python <br><br><br><br><br><br><br><br><br><br>"],
+      'duration': '',
+      'link': 'https://github.com/ameyak11/Creative-prims'
     }
+    
 
   ]
 
@@ -63,6 +111,7 @@ $(document).ready(function () {
     });
     $('.modal-content').append('</ul>');
 
+    $('.modal-subtitle').text(data[id].subtitle);
     $('.modal-duration').text(data[id].duration);
 
     $('.modal-color').attr('class', 'uk-background-cover modal-color');
